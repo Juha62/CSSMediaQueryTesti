1a75a9dba075db487057a3eab0ccc97d786f6e44
Juha Pusa <juha.m.pusa@hotmail.com>
2017-04-15 12:02:20 +0300

    MVC-Projektin luonti

---
+++ CSSMediaQueryTest/CSSMediaQueryTest/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CSSMediaQueryTest.Startup))]
+namespace CSSMediaQueryTest
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
